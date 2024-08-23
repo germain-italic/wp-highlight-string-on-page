d317a889939f6b172b0d40d30926117a84ef54dd
Germain Metti <germain@italic.fr>
2024-08-23 17:18:34 +0400

    highlight all occurrences at once

--- wp-highlight-string-on-page.php
+++ wp-highlight-string-on-page.php
@@ -9,12 +9,12 @@ Author: Germain-Italic
 function highlight_search_shortcode() {
     ob_start();
     ?>
-    <input type="text" id="searchInput" placeholder="Enter text to search">
-    <button id="searchButton">Search</button>
+    <input type="text" id="wphsop-searchInput" placeholder="Enter text to search">
+    <button id="wphsop-searchButton">Search</button>
 
     <script>
-    document.getElementById('searchButton').addEventListener('click', function() {
-        var searchValue = document.getElementById('searchInput').value;
+    document.getElementById('wphsop-searchButton').addEventListener('click', function() {
+        var searchValue = document.getElementById('wphsop-searchInput').value;
         if (searchValue) {
             removeHighlights();
             highlightText(searchValue);
@@ -25,20 +25,26 @@ function highlight_search_shortcode() {
         var regex = new RegExp(searchValue, 'gi');
         var walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
         var node;
+        var matches = [];
 
+        // Collect all matches first
         while (node = walker.nextNode()) {
-            var parent = node.parentNode;
-            if (parent && parent.nodeName !== 'SCRIPT' && parent.nodeName !== 'STYLE') {
-                var matches = node.nodeValue.match(regex);
-                if (matches) {
-                    var span = document.createElement('span');
-                    span.innerHTML = node.nodeValue.replace(regex, function(matched) {
-                        return '<mark>' + matched + '</mark>';
-                    });
-                    parent.replaceChild(span, node);
+            if (node.parentNode && node.parentNode.nodeName !== 'SCRIPT' && node.parentNode.nodeName !== 'STYLE') {
+                if (regex.test(node.nodeValue)) {
+                    matches.push(node);
                 }
             }
         }
+
+        // Now highlight the collected matches
+        matches.forEach(function(node) {
+            var parent = node.parentNode;
+            var span = document.createElement('span');
+            span.innerHTML = node.nodeValue.replace(regex, function(matched) {
+                return '<mark>' + matched + '</mark>';
+            });
+            parent.replaceChild(span, node);
+        });
     }
 
     function removeHighlights() {
